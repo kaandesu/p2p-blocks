04d4408f20766c04c80baa2ff965ca4eab875623
kaan <kaandesu00@gmail.com>
2025-03-15 11:47:58 +0100

    chore: define proof of work

---
+++ blockchain/proof.go
@@ -0,0 +1,68 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log/slog"
+	"math"
+	"math/big"
+)
+
+const (
+	Difficulty = 18
+)
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+func NewProof(b *Block) (pow *ProofOfWork) {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+	pow = &ProofOfWork{b, target}
+	return
+}
+
+func (pow *ProofOfWork) InitData(nonce int) (data []byte) {
+	data = bytes.Join([][]byte{
+		pow.Block.PrevHash,
+		pow.Block.Data,
+		ToHex(int64(nonce)),
+		ToHex(int64(Difficulty)),
+	}, []byte{})
+	return
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var intHash big.Int
+	var hash [32]byte
+	nonce := 0
+
+	for nonce < math.MaxInt64 {
+		data := pow.InitData(nonce)
+		hash = sha256.Sum256(data)
+
+		fmt.Printf("\r%x", hash)
+		intHash.SetBytes(hash[:])
+
+		if intHash.Cmp(pow.Target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Println()
+
+	return nonce, hash[:]
+}
+
+func ToHex(num int64) []byte {
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, binary.BigEndian, num); err != nil {
+		slog.Error("could not convert ToHex", "err", err)
+	}
+	return buf.Bytes()
+}
